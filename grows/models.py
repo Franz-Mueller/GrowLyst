8eebe2c7a123426460f39deb1c98b7724c16dfa7
Franz-Mueller <franzmueller2003@outlook.de>
2025-06-23 22:01:32 +0200

    Add models for Grow, Environment, Measurement, Unit, and Plant with relationships

--- grows/models.py
+++ grows/models.py
@@ -1,3 +1,209 @@
 from django.db import models
 
-# Create your models here.
+
+class Grow(models.Model):
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="grows",
+        blank=True,
+        null=True,
+    )
+    type = models.CharField(
+        max_length=50,
+        choices=[
+            ("flower", "Flower"),
+            ("mothers", "Mothers"),
+            ("breeding", "Breeding"),
+            ("other", "Other"),
+        ],
+        default="flower",
+    )
+    name = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(
+        upload_to="grows/", blank=True, null=True
+    )  # TODO Implement image upload
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Grow for {self.user.username if self.user else 'Unknown User'}"
+
+
+class Environment(models.Model):
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="grows",
+        blank=True,
+        null=True,
+    )
+    name = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(
+        upload_to="environments/", blank=True, null=True
+    )  # TODO Implement image upload
+
+    def __str__(self):
+        return f"Environment for {self.grow.user.username if self.grow else 'Unknown User'}"
+
+
+class Measurement(models.Model):
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="measurements",
+        blank=True,
+        null=True,
+    )
+    grow = models.ForeignKey(
+        Grow,
+        on_delete=models.CASCADE,
+        related_name="measurements",
+        blank=True,
+        null=True,
+    )
+    environment = models.ForeignKey(
+        Environment,
+        on_delete=models.CASCADE,
+        related_name="measurements",
+        blank=True,
+        null=True,
+    )
+    plant = models.ForeignKey(
+        "plants.Plant",
+        on_delete=models.CASCADE,
+        related_name="measurements",
+        blank=True,
+        null=True,
+    )
+    unit = models.ForeignKey(
+        "Unit",
+        on_delete=models.CASCADE,
+        related_name="measurements",
+        blank=True,
+        null=True,
+    )
+    value = models.FloatField(blank=True, null=True)
+    unit = models.CharField(max_length=50, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Measurement {self.name} for {self.grow.name if self.grow else 'Unknown Grow'}"
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    symbol = models.CharField(max_length=10, unique=True)
+    conversion_factor_to_base = models.FloatField(default=1.0)
+
+    def __str__(self):
+        return f"{self.name} ({self.symbol})"
+
+
+class Unittype(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+    icon = models.ImageField(
+        upload_to="unit_types/", blank=True, null=True
+    )  # TODO Implement image upload
+    baseunit = models.ForeignKey(
+        "Unit",
+        on_delete=models.CASCADE,
+        related_name="baseunit",
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Actioncategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+    icon = models.ImageField(
+        upload_to="action_categories/", blank=True, null=True
+    )  # TODO Implement image upload
+
+    def __str__(self):
+        return self.name
+
+
+class Actiontype(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+    icon = models.ImageField(
+        upload_to="action_types/", blank=True, null=True
+    )  # TODO Implement image upload
+    category = models.ForeignKey(
+        Actioncategory,
+        on_delete=models.CASCADE,
+        related_name="action_types",
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Action(models.Model):
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="actions",
+        blank=True,
+        null=True,
+    )
+    grow = models.ForeignKey(
+        Grow,
+        on_delete=models.CASCADE,
+        related_name="actions",
+        blank=True,
+        null=True,
+    )
+    environment = models.ForeignKey(
+        Environment,
+        on_delete=models.CASCADE,
+        related_name="actions",
+        blank=True,
+        null=True,
+    )
+    plant = models.ForeignKey(
+        "plants.Plant",
+        on_delete=models.CASCADE,
+        related_name="actions",
+        blank=True,
+        null=True,
+    )
+    Actioncategory = models.ForeignKey(
+        Actioncategory,
+        on_delete=models.CASCADE,
+        related_name="actions",
+        blank=True,
+        null=True,
+    )
+    action_type = models.ForeignKey(
+        Actiontype,
+        on_delete=models.CASCADE,
+        related_name="actions",
+        blank=True,
+        null=True,
+    )
+    description = models.TextField(blank=True, null=True)
+    before_image = models.ImageField(
+        upload_to="actions/before/", blank=True, null=True
+    )  # TODO Implement image upload
+    after_image = models.ImageField(
+        upload_to="actions/after/", blank=True, null=True
+    )  # TODO Implement image upload
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Action {self.action_type.name} for {self.grow.name if self.grow else 'Unknown Grow'}"
--- plants/models.py
+++ plants/models.py
@@ -1,3 +1,157 @@
 from django.db import models
 
-# Create your models here.
+
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    strain = models.OneToOneField(
+        "Strain", on_delete=models.CASCADE, related_name="plant", blank=True, null=True
+    )
+    environment = models.OneToOneField(
+        "Environment",
+        on_delete=models.CASCADE,
+        related_name="plant",
+        blank=True,
+        null=True,
+    )
+    grow = models.OneToOneField(
+        "Grow", on_delete=models.CASCADE, related_name="plant", blank=True, null=True
+    )
+    user = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        related_name="plants",
+        blank=True,
+        null=True,
+    )
+    current_stage = models.ForeignKey(
+        "Stage",
+        on_delete=models.CASCADE,
+        related_name="current_plants",
+        blank=True,
+        null=True,
+    )
+    mediumtype = models.ForeignKey(
+        "Mediumtype",
+        on_delete=models.CASCADE,
+        related_name="plants",
+        blank=True,
+        null=True,
+    )
+    medium = models.ForeignKey(
+        "Medium",
+        on_delete=models.CASCADE,
+        related_name="plants",
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(
+        upload_to="plants/", blank=True, null=True
+    )  # TODO implement Image
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    image = models.ImageField(upload_to="plants/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Plantstage(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+
+class Plantstagechange(models.Model):
+    user = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        related_name="plant_stage_changes",
+        blank=True,
+        null=True,
+    )
+    plant = models.ForeignKey(
+        "Plant",
+        on_delete=models.CASCADE,
+        related_name="stage_changes",
+        blank=True,
+        null=True,
+    )
+    plantstage = models.ForeignKey(
+        "Plantstage", on_delete=models.CASCADE, related_name="changes"
+    )
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Strain(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    breeder = models.ForeignKey(
+        "Breeder",
+        on_delete=models.CASCADE,
+        related_name="strains",
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(
+        upload_to="strains/", blank=True, null=True
+    )  # TODO implement Image
+
+
+class Mediumtype(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Medium(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    medium_type = models.ForeignKey(
+        "Mediumtype",
+        on_delete=models.CASCADE,
+        related_name="mediums",
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Breeder(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    image = models.ImageField(
+        upload_to="breeders/", blank=True, null=True
+    )  # TODO implement Image
+
+    def __str__(self):
+        return self.name
+
+
+class Plantphoto(models.Model):
+    user = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        related_name="plant_photos",
+        blank=True,
+        null=True,
+    )
+    plant = models.ForeignKey(
+        "Plant",
+        on_delete=models.CASCADE,
+        related_name="photos",
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(upload_to="plant_photos/", blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Photo for {self.plant.name if self.plant else 'Unknown Plant'}"
