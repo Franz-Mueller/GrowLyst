37538b4abb059b5fb367ab295feca9b8764c3f51
Franz-Mueller <franzmueller2003@outlook.de>
2025-06-30 21:36:34 +0200

    Add templates for measurement types, media, nutrients, plant stages, and units
    - Created individual detail and list templates for measurement types, media, nutrients, plant stages, and units.
    - Implemented responsive design using Bootstrap classes for better UI.
    - Added navigation buttons to return to the respective lists.
    - Included styling for clickable table rows to enhance user experience.
    - Ensured proper data rendering for each entity with relevant fields displayed.

--- plants/views.py
+++ plants/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 
-from .models import Plant, Grow, Measurement, Environment, Group
+from .models import *
 
 
 @login_required
@@ -60,3 +60,255 @@ def group(request, group_id):
     group = Group.objects.get(id=group_id)
     content = {"group": group}
     return render(request, "plants/group.html", content)
+
+
+@login_required
+def plantstages(request):
+    plantstages = Plantstage.objects.for_user(request.user)
+    content = {"plantstages": plantstages, "section": "plantstages"}
+    return render(request, "plants/plantstages.html", content)
+
+
+@login_required
+def plantstage(request, plantstage_id):
+    plantstage = Plantstage.objects.get(id=plantstage_id)
+    content = {"plantstage": plantstage}
+    return render(request, "plants/plantstage.html", content)
+
+
+@login_required
+def plantstagelogs(request):
+    plantstagelogs = Plantstagelog.objects.for_user(request.user)
+    content = {"plantstagelogs": plantstagelogs, "section": "plantstagelogs"}
+    return render(request, "plants/plantstagelogs.html", content)
+
+
+@login_required
+def plantstagelog(request, plantstagelog_id):
+    plantstagelog = Plantstagelog.objects.get(id=plantstagelog_id)
+    content = {"plantstagelog": plantstagelog}
+    return render(request, "plants/plantstagelog.html", content)
+
+
+@login_required
+def strains(request):
+    strains = Strain.objects.for_user(request.user)
+    content = {"strains": strains, "section": "strains"}
+    return render(request, "plants/strains.html", content)
+
+
+@login_required
+def strain(request, strain_id):
+    strain = Strain.objects.get(id=strain_id)
+    content = {"strain": strain}
+    return render(request, "plants/strain.html", content)
+
+
+@login_required
+def breeders(request):
+    breeders = Breeder.objects.for_user(request.user)
+    content = {"breeders": breeders, "section": "breeders"}
+    return render(request, "plants/breeders.html", content)
+
+
+@login_required
+def breeder(request, breeder_id):
+    breeder = Breeder.objects.get(id=breeder_id)
+    content = {"breeder": breeder}
+    return render(request, "plants/breeder.html", content)
+
+
+@login_required
+def mediumtypes(request):
+    mediumtypes = Mediumtype.objects.for_user(request.user)
+    content = {"mediumtypes": mediumtypes, "section": "mediumtypes"}
+    return render(request, "plants/mediumtypes.html", content)
+
+
+@login_required
+def mediumtype(request, mediumtype_id):
+    mediumtype = Mediumtype.objects.get(id=mediumtype_id)
+    content = {"mediumtype": mediumtype}
+    return render(request, "plants/mediumtype.html", content)
+
+
+@login_required
+def media(request):
+    media = Medium.objects.for_user(request.user)
+    content = {"media": media, "section": "media"}
+    return render(request, "plants/media.html", content)
+
+
+@login_required
+def medium(request, medium_id):
+    medium = Medium.objects.get(id=medium_id)
+    content = {"medium": medium}
+    return render(request, "plants/medium.html", content)
+
+
+@login_required
+def plantphotos(request):
+    plantphotos = Plantphoto.objects.for_user(request.user)
+    content = {"plantphotos": plantphotos, "section": "plantphotos"}
+    return render(request, "plants/plantphotos.html", content)
+
+
+@login_required
+def plantphoto(request, plantphoto_id):
+    plantphoto = Plantphoto.objects.get(id=plantphoto_id)
+    content = {"plantphoto": plantphoto}
+    return render(request, "plants/plantphoto.html", content)
+
+
+@login_required
+def growtypes(request):
+    growtypes = Growtype.objects.for_user(request.user)
+    content = {"growtypes": growtypes, "section": "growtypes"}
+    return render(request, "plants/growtypes.html", content)
+
+
+@login_required
+def growtype(request, growtype_id):
+    growtype = Growtype.objects.get(id=growtype_id)
+    content = {"growtype": growtype}
+    return render(request, "plants/growtype.html", content)
+
+
+@login_required
+def environments_list(request):
+    environments = Environment.objects.for_user(request.user)
+    content = {"environments": environments, "section": "environments"}
+    return render(request, "plants/environments.html", content)
+
+
+@login_required
+def environment_detail(request, environment_id):
+    environment = Environment.objects.get(id=environment_id)
+    content = {"environment": environment}
+    return render(request, "plants/environment.html", content)
+
+
+@login_required
+def measurements(request):
+    measurements = Measurement.objects.for_user(request.user)
+    content = {"measurements": measurements, "section": "measurements"}
+    return render(request, "plants/measurements.html", content)
+
+
+@login_required
+def measurement(request, measurement_id):
+    measurement = Measurement.objects.get(id=measurement_id)
+    content = {"measurement": measurement}
+    return render(request, "plants/measurement.html", content)
+
+
+@login_required
+def units(request):
+    units = Unit.objects.for_user(request.user)
+    content = {"units": units, "section": "units"}
+    return render(request, "plants/units.html", content)
+
+
+@login_required
+def unit(request, unit_id):
+    unit = Unit.objects.get(id=unit_id)
+    content = {"unit": unit}
+    return render(request, "plants/unit.html", content)
+
+
+@login_required
+def unittypes(request):
+    unittypes = Unittype.objects.for_user(request.user)
+    content = {"unittypes": unittypes, "section": "unittypes"}
+    return render(request, "plants/unittypes.html", content)
+
+
+@login_required
+def unittype(request, unittype_id):
+    unittype = Unittype.objects.get(id=unittype_id)
+    content = {"unittype": unittype}
+    return render(request, "plants/unittype.html", content)
+
+
+@login_required
+def actioncategories(request):
+    actioncategories = Actioncategory.objects.for_user(request.user)
+    content = {"actioncategories": actioncategories, "section": "actioncategories"}
+    return render(request, "plants/actioncategories.html", content)
+
+
+@login_required
+def actioncategory(request, actioncategory_id):
+    actioncategory = Actioncategory.objects.get(id=actioncategory_id)
+    content = {"actioncategory": actioncategory}
+    return render(request, "plants/actioncategory.html", content)
+
+
+@login_required
+def actiontypes(request):
+    actiontypes = Actiontype.objects.for_user(request.user)
+    content = {"actiontypes": actiontypes, "section": "actiontypes"}
+    return render(request, "plants/actiontypes.html", content)
+
+
+@login_required
+def actiontype(request, actiontype_id):
+    actiontype = Actiontype.objects.get(id=actiontype_id)
+    content = {"actiontype": actiontype}
+    return render(request, "plants/actiontype.html", content)
+
+
+@login_required
+def actionlogs(request):
+    actionlogs = ActionLog.objects.for_user(request.user)
+    content = {"actionlogs": actionlogs, "section": "actionlogs"}
+    return render(request, "plants/actionlogs.html", content)
+
+
+@login_required
+def actionlog(request, actionlog_id):
+    actionlog = ActionLog.objects.get(id=actionlog_id)
+    content = {"actionlog": actionlog}
+    return render(request, "plants/actionlog.html", content)
+
+
+@login_required
+def nutrients(request):
+    nutrients = Nutrient.objects.for_user(request.user)
+    content = {"nutrients": nutrients, "section": "nutrients"}
+    return render(request, "plants/nutrients.html", content)
+
+
+@login_required
+def nutrient(request, nutrient_id):
+    nutrient = Nutrient.objects.get(id=nutrient_id)
+    content = {"nutrient": nutrient}
+    return render(request, "plants/nutrient.html", content)
+
+
+@login_required
+def nutritions(request):
+    nutritions = Nutrition.objects.for_user(request.user)
+    content = {"nutritions": nutritions, "section": "nutritions"}
+    return render(request, "plants/nutritions.html", content)
+
+
+@login_required
+def nutrition(request, nutrition_id):
+    nutrition = Nutrition.objects.get(id=nutrition_id)
+    content = {"nutrition": nutrition}
+    return render(request, "plants/nutrition.html", content)
+
+
+@login_required
+def measurementtypes(request):
+    measurementtypes = Measurementtype.objects.for_user(request.user)
+    content = {"measurementtypes": measurementtypes, "section": "measurementtypes"}
+    return render(request, "plants/measurementtypes.html", content)
+
+
+@login_required
+def measurementtype(request, measurementtype_id):
+    measurementtype = Measurementtype.objects.get(id=measurementtype_id)
+    content = {"measurementtype": measurementtype}
+    return render(request, "plants/measurementtype.html", content)
